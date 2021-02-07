81752fe86bf1cac946c0f2778d5d7370e1dc7685
DJChip <thienthantinhyeu968@gmail.com>
2021-02-07 21:38:32 +0700

    Add traveler

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+ devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_one_attached :avatar
+  has_many :likes,  dependent: :destroy
+  has_many :post
+         
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :hobby, presence: true
+  validates :local, presence: true
+end
